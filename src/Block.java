c048920e35659af2c1746a6fe4c44139e962e712
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-04-28 15:53:03 +0000

    update readMe

--- src/Block.java
+++ src/Block.java
@@ -5,6 +5,7 @@ public class Block {
     public String previousHash;
     private final String data; // data will be a simple message.
     private final long timeStamp; // as number of milliseconds since 1/1/1970.
+    private int nonce;
 
     // Block Constructor.
     public Block(String data, String previousHash) {
@@ -16,7 +17,20 @@ public class Block {
 
     // Applies Sha256 to a string and returns the result.
     public String calculatehash(){
-        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+        String calculatedHash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
+              data);
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculatehash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
